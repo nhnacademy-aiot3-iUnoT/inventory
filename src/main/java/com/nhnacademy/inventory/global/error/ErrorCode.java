fd3310cdc87279d778dd5531f130f6572e2ffe88
{gogooga1} <{dudrn322056@naver.com}>
2026-07-28 13:28:33 +0900

    feat : 구역 crud 기능 및 api 구

--- src/main/java/com/nhnacademy/inventory/global/error/ErrorCode.java
+++ src/main/java/com/nhnacademy/inventory/global/error/ErrorCode.java
@@ -15,7 +15,11 @@ public enum ErrorCode {
 
     //저장소 에러코드
     STORAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "S001", "존재하지 않는 저장소입니다."),
-    STORAGE_NAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "S002", "이미 존재하는 저장소 이름입니다.");
+    STORAGE_NAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "S002", "조직내의 이미 존재하는 저장소 이름입니다."),
+
+    //구역 에러코드
+    ZONE_NOT_FOUND(HttpStatus.NOT_FOUND, "Z001", "존재하지 않는 구역입니다."),
+    ZONE_NAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "Z002", "저장소내의 이미 존재하는 구역 이름입니다.");
 
     private final HttpStatus status;
     private final String code;
