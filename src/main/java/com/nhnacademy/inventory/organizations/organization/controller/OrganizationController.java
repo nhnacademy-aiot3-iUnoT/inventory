1b23ed40aae20da9f2296c431b5ff3e81577f217
konayeong <dnjfrhkd402@naver.com>
2026-08-09 00:00:13 +0900

    refactor : /me -> 공통처리

--- src/main/java/com/nhnacademy/inventory/organizations/organization/controller/OrganizationController.java
+++ src/main/java/com/nhnacademy/inventory/organizations/organization/controller/OrganizationController.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/core/organizations")
+@RequestMapping("/api/core/organizations/me")
 public class OrganizationController {
     private final OrganizationService organizationService;
 
     /**
      * 조직 정보 조회
      */
-    @GetMapping("/me")
+    @GetMapping("")
     public ResponseEntity<ApiResponse<OrgDetailResponse>> getOrganizationInfo(){
         OrgDetailResponse organizationForUser = organizationService.getOrganizationForUser();
         return ResponseEntity.ok(ApiResponse.success(organizationForUser));
@@ -29,7 +29,7 @@ public class OrganizationController {
     /**
      * 조직 상태 활성화 <-> 비활성화 (OWNER 체크)
      */
-    @PutMapping("/me/status")
+    @PutMapping("/status")
     public ResponseEntity<Void> updateOrganizationStatus(@RequestBody @Valid OrgStatusUpdateRequest request) {
         organizationService.updateOrganizationStatus(request);
         return ResponseEntity.noContent().build();
@@ -38,7 +38,7 @@ public class OrganizationController {
     /**
      * 조직 정보 수정 (주소, 상세설명)
      */
-    @PutMapping("/me")
+    @PutMapping()
     public ResponseEntity<Void> updateOrganization(@RequestBody @Valid OrgUpdateRequest request) {
         organizationService.updateOrganization(request);
         return ResponseEntity.noContent().build();
@@ -47,7 +47,7 @@ public class OrganizationController {
     /**
      * 조직 초기화 (admin이 조직 생성 후 owner 최초 입력)
      */
-    @PutMapping("/me/setup")
+    @PutMapping("/setup")
     public ResponseEntity<Void> setupOrganization(@RequestBody @Valid OrganizationSetupRequest request) {
         organizationService.setupOrganization(request);
         return ResponseEntity.noContent().build();
