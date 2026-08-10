f5a485d3b550bc710201db2037e00fdfb3d1cdb8
konayeong <133228490+konayeong@users.noreply.github.com>
2026-08-10 15:28:33 +0900

    fix : '/' 추가

--- src/main/java/com/nhnacademy/inventory/global/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/inventory/global/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/actuator/health", "/actuator/health/**", "/actuator/serviceregistry"
                         ).permitAll()
-                        .requestMatchers("api/core/internal/**").permitAll()
+                        .requestMatchers("/api/core/internal/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
