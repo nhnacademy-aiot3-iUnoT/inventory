c2b0f81d5041a3648494f240b4e860b278b4fb3a
Splleat <Splleat@gmail.com>
2026-08-12 17:41:55 +0900

    chore: Spring AI 설정

--- src/main/java/com/nhnacademy/inventory/InventoryApplication.java
+++ src/main/java/com/nhnacademy/inventory/InventoryApplication.java
@@ -3,11 +3,12 @@ package com.nhnacademy.inventory;
 import com.nhnacademy.inventory.global.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableResilientMethods
 @EnableAsync
 @EnableConfigurationProperties(JwtProperties.class)
 public class InventoryApplication {
