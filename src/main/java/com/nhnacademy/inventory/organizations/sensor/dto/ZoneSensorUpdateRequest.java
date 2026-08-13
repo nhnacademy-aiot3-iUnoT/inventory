fe9c0ebd150e5a89a8ef8c22fd681ec93a6e4861
{gogooga1} <{dudrn322056@naver.com}>
2026-08-13 10:18:40 +0900

    refactor : @max 사이즈 수정

--- src/main/java/com/nhnacademy/inventory/organizations/sensor/dto/ZoneSensorUpdateRequest.java
+++ src/main/java/com/nhnacademy/inventory/organizations/sensor/dto/ZoneSensorUpdateRequest.java
@@ -8,7 +8,7 @@ public record ZoneSensorUpdateRequest(
         @Size(max = 50, message = "이름은 최대 50자 입니다.")
         String name,
 
-        @Size(max = 50, message = "설명은 최대 255자 입니다.")
+        @Size(max = 255, message = "설명은 최대 255자 입니다.")
         String description
 ) {
 }
