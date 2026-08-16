c13211e17cd2fe9fd041b9dcae564221f900e422
Splleat <splleat@gmail.com>
2026-08-16 20:41:44 +0900

    chore: 사용하지 않는 의존성 제거

--- src/main/java/com/nhnacademy/inventory/InventoryApplication.java
+++ src/main/java/com/nhnacademy/inventory/InventoryApplication.java
@@ -4,7 +4,6 @@ import com.nhnacademy.inventory.global.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.resilience.annotation.EnableResilientMethods;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
