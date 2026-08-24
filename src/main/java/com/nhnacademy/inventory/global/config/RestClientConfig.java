570225afded5e986a676a1c95a0076581087631c
Splleat <Splleat@gmail.com>
2026-08-24 14:14:29 +0900

    fix: Eureka가 사용할 RestClient.Builder를 Bean으로 등록

--- src/main/java/com/nhnacademy/inventory/global/config/RestClientConfig.java
+++ src/main/java/com/nhnacademy/inventory/global/config/RestClientConfig.java
@@ -7,12 +7,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties(MedicineProperties.class)
 public class RestClientConfig {
+
+    @Bean
+    @Primary
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 
     @Bean("medicineRestClient")
     public RestClient medicineRestClient(MedicineProperties medicineProperties) {
